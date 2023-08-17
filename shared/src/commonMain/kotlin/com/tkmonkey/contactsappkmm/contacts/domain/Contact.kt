90a118c85ac31b6be8411757663831bd906a0fd9
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-08-16 22:15:58 -0500

    Create contact list events and state

---
+++ shared/src/commonMain/kotlin/com/tkmonkey/contactsappkmm/contacts/domain/Contact.kt
@@ -0,0 +1,10 @@
+package com.tkmonkey.contactsappkmm.contacts.domain
+
+data class Contact(
+	val id: Long?,
+	val firstName: String,
+	val lastName: String,
+	val email: String,
+	val phoneNumber: String,
+	val photoByte: ByteArray?
+)
---
+++ shared/src/commonMain/kotlin/com/tkmonkey/contactsappkmm/contacts/presentation/ContactListEvent.kt
@@ -0,0 +1,20 @@
+package com.tkmonkey.contactsappkmm.contacts.presentation
+
+import com.tkmonkey.contactsappkmm.contacts.domain.Contact
+
+sealed interface ContactListEvent {
+	object OnAddNewContactClick: ContactListEvent
+	object DismissContact: ContactListEvent
+	data class OnFirstNameChange(val firstName: String): ContactListEvent
+	data class OnLastNameChange(val lastName: String): ContactListEvent
+	data class OnEmailChange(val email: String): ContactListEvent
+	data class OnPhoneNumberChange(val phoneNumber: String): ContactListEvent
+	 class OnPhotoPicked(val bytes: ByteArray): ContactListEvent
+	object OnAddPhotoClicked: ContactListEvent
+	object SaveContact: ContactListEvent
+	data class SelectContact(val contact: Contact): ContactListEvent
+	data class EditContact(val contact: Contact): ContactListEvent
+	object DeleteContact: ContactListEvent
+
+
+}
