3ca75c46e64b479b2058249ec7349de613aae42f
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-08-16 21:43:00 -0500

    Add initial libs config

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
     id("com.android.library").version("8.1.0").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
+    id("org.jetbrains.compose").version("1.5.0-beta02").apply(false)
+
 }
 
 tasks.register("clean", Delete::class) {
